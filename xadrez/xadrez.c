97f45e39231b31b8a2baf9dffb57be867c004ed0
bolaDev <60231147+bolaDev@users.noreply.github.com>
2026-03-01 19:46:08 +0000

    criei estrutura de condição doWhile para movimento da Rainha

--- xadrez/xadrez.c
+++ xadrez/xadrez.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    
+    //variáveis de controle
     int i = 0;
-    int j;
+    int j = 0;
 
     printf("Desafio Xadrez!\n");
     printf("Movimento da Torre\n");
 
+    //lógica movimento Torre
     for(int i = 0; i < 5; i++){
         printf("Direita\n");
     }
     printf("--------------------\n");
     printf("Movimento do Bispo\n");
 
+
+    //lógica movimento Bispo
     while (i < 5)
     {
         printf("Cima e Direita\n");
         i++;
     }
-    
-    
+
+    printf("--------------------\n");
+    printf("Movimento da Rainha\n");
+
+    do
+    {
+        printf("Esquerda\n");
+        j++;
+    }while (j < 8);
+   
     return 0;
 }
