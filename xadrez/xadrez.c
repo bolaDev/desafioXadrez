0cf1bf2088b74346657527f34082c595228cf04c
bolaDev <60231147+bolaDev@users.noreply.github.com>
2026-03-04 01:20:32 +0000

    Implementei lógica para movimento da peça "Cavalo"

--- xadrez/xadrez.c
+++ xadrez/xadrez.c
@@ -31,6 +31,75 @@ int main(){
         printf("Esquerda\n");
         j++;
     }while (j < 8);
-   
+
+    //Movimento do Cavalo
+
+    int movimento = 1;
+    int opcao;
+    printf("Escolha para onde será o movimento da peça:\n");
+    printf("1. Esquerda\n");
+    printf("2. Direita\n");
+    printf("3. Cima\n");
+    printf("4. Baixo\n");
+    scanf("%d",&opcao);
+
+    switch (opcao )
+    {
+    case 1:
+        while (movimento < 2)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                printf("Esquerda\n"); // Imprimirá 'Esquerda' 2 vezes
+            }
+
+            printf("Cima\n"); // Imprirá 'Cima' 1 vez
+            movimento++;
+        }
+        break;
+
+        case 2:
+        while (movimento < 2)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                printf("Direita\n"); // Imprimirá 'Direita' 2 vezes
+            }
+
+            printf("Baixo\n"); // Imprirá 'Baixo' 1 vez
+            movimento++;
+        }
+        break;
+
+        case 3:
+        while (movimento < 2)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                printf("Cima\n"); // Imprimirá 'Cima' 2 vezes
+            }
+
+            printf("Esquerda\n"); // Imprirá 'Esquerda' 1 vez
+            movimento++;
+        }
+        break;
+
+        case 4:
+        while (movimento < 2)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                printf("Baixo\n"); // Imprimirá 'Baixo' 2 vezes
+            }
+
+            printf("Direita\n"); // Imprirá 'Direita' 1 vez
+            movimento++;
+        }
+        break;
+    
+    default:
+        break;
+    }
+
     return 0;
 }
